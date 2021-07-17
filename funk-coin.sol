545f55d11a63a49bbd38ae1fad1b35273260b968
Spencer Fairchild <70547155+fairchildspencer@users.noreply.github.com>
2021-07-17 12:50:26 -0600

    Added safeMath to improve security

--- funk-coin.sol
+++ funk-coin.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.7.0;
 
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a — b;
+    }
+    function add(uint256 a, uint256 b) internal pure returns (uint256)   {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
+}
+
+using SafeMath for uint256;
+
 contract FunkCoin {
 
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -10,6 +24,9 @@ contract FunkCoin {
     mapping(address => mapping (address => uint256)) allowed; 
 
     uint256 totalSupply_;
+    string public constant name = "Funk Coin";
+    string public constant symbol = "FUNK";
+    uint8 public constant decimals = 18;
 
     constructor(uint256 totalTokens) {
         totalSupply_ = totalTokens;
@@ -27,8 +44,8 @@ contract FunkCoin {
     function transfer(address receiver, uint numTokens) public returns (bool) {
         require(numTokens <= balances[msg.sender]);
 
-        balances[msg.sender] = balances[msg.sender] - numTokens;
-        balances[receiver] = balances[receiver] + numTokens;
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
 
         emit Transfer(msg.sender, receiver, numTokens);
         return true;
@@ -49,9 +66,9 @@ contract FunkCoin {
         require(numTokens <= balances[owner]);
         require(numTokens <= allowed[owner][msg.sender]);
 
-        balances[owner] = balances[owner] - numTokens;
+        balances[owner] = balances[owner].sub(numTokens);
         allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
-        balances[buyer] = balances[buyer] + numTokens;
+        balances[buyer] = balances[buyer].add(numTokens);
         
         Transfer(owner, buyer, numTokens);
         return true;
